f2b2b7590a9fd260ca56bd672437991d50a7b14f
Qyupang <hyunkyu0509@naver.com>
2022-10-10 11:39:42 +0900

    Add README.md

--- 1week/index.js
+++ 1week/index.js
@@ -3,3 +3,12 @@ const menuBar = document.querySelector('.fa-bars');
 const handleMenuBar = () => console.log('hello');
 
 menuBar.addEventListener('mouseover', handleMenuBar);
+
+// const bannerDescript = document.querySelectorAll('.banner-descript');
+
+// console.log(bannerDescript);
+
+// const handlebanner = () => bannerDescript.classList.toggle('hidden');
+
+// bannerDescript.
+// addEventListener('mouseover', handlebanner);
