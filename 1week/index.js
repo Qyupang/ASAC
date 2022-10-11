3dd60fa268c09bd28de4311822a7f15a93ac66c7
Qyupang <hyunkyu0509@naver.com>
2022-10-11 16:35:44 +0900

    Feat: add buffering

--- 1week/index.js
+++ 1week/index.js
@@ -12,3 +12,10 @@ menuBar.addEventListener('mouseover', handleMenuBar);
 
 // bannerDescript.
 // addEventListener('mouseover', handlebanner);
+
+setTimeout(() => {
+  const hiddens = document.querySelectorAll('.hidden');
+  hiddens.forEach((hidden) => hidden.classList.toggle('hidden'));
+  const spinner = document.querySelector('.spinner');
+  spinner.classList.toggle('hidden');
+}, 3000);
