829b25e16a8f5c247d0da0d25958cd65400a105f
Qyupang <hyunkyu0509@naver.com>
2022-10-11 17:53:16 +0900

    Upload Practice Files

---
+++ 2week/practice/todo.js
@@ -0,0 +1,20 @@
+let inputBox = document.getElementById('inputBox');
+let addTodo = document.getElementById('addTodo');
+let todoList = document.getElementById('todoList');
+
+addTodo.addEventListener('click', function () {
+  const list = document.createElement('li');
+  list.classList.add('list');
+  if (!inputBox.value) alert('할일을 입력해주세요!');
+  else {
+    list.innerText = inputBox.value;
+    todoList.appendChild(list);
+    inputBox.value = '';
+  }
+  list.addEventListener('click', function () {
+    list.style.textDecoration = 'line-through';
+  });
+  list.addEventListener('dblclick', function () {
+    todoList.removeChild(list);
+  });
+});
