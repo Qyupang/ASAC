c3a517c85d7fe78adc9c5c4c386eb3033d79a222
Qyupang <hyunkyu0509@naver.com>
2022-10-15 23:04:58 +0900

    Feat : Add confirm in modal

--- 2week/clone_coding/js/modal.js
+++ 2week/clone_coding/js/modal.js
@@ -2,6 +2,8 @@ const loginButton = document.querySelector('.login-button');
 const xmarks = document.querySelectorAll('.fa-xmark');
 const emailButton = document.querySelector('.login-modal__body-button-email');
 
+const checkAll = document.getElementsByName('agree-all');
+
 const handleModalWithBtn = function () {
   const modal = document.querySelector('.modal');
   const body = document.querySelector('body');
@@ -26,18 +28,28 @@ const handleModalWithX = function () {
   const modalRegister = document.querySelector('.register-modal');
   const body = document.querySelector('body');
 
-  modal.classList.add('modal-hidden');
   if (!modalLogin.classList.contains('modal-hidden')) {
+    modal.classList.add('modal-hidden');
     modalLogin.classList.toggle('modal-hidden');
+    body.classList.toggle('modal-body');
   }
   if (!modalRegister.classList.contains('modal-hidden')) {
-    modalRegister.classList.toggle('modal-hidden');
+    if (confirm('회원가입을 취소하겠습니까?')) {
+      modalRegister.classList.toggle('modal-hidden');
+      modal.classList.add('modal-hidden');
+      body.classList.toggle('modal-body');
+    }
   }
-  body.classList.toggle('modal-body');
+};
+
+const handleCheckAll = function () {
+  console.log('clicked');
 };
 
 loginButton.addEventListener('click', handleModalWithBtn);
-for (xmark of xmarks) {
+for (let xmark of xmarks) {
   xmark.addEventListener('click', handleModalWithX);
 }
 emailButton.addEventListener('click', handelSecondModal);
+
+checkAll.addEventListener('click', handleCheckAll);
