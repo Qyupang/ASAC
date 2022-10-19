648fd41d1acb84ce0b699d1530d69d6b142d70e7
Qyupang <hyunkyu0509@naver.com>
2022-10-19 09:43:28 +0900

    Feat : Add Header in React

---
+++ 3week/clone_coding/wanted_clone/src/Banner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+function Banner() {
+  return (
+    
+  );
+}
+
+export default Banner;
---
+++ 3week/clone_coding/wanted_clone/src/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './style.css';
+import './header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="left-side">
+        <i className="fa-solid fa-bars cursor"></i>
+        <h1 className="logo cursor">
+          <a href="index.html">wanted</a>
+        </h1>
+      </div>
+      <nav className="header-nav">
+        <ul>
+          <li>
+            <span className="none">
+              <a href="recruit.html">채용</a>
+            </span>
+          </li>
+          <li>
+            <span className="none">이벤트 </span>
+          </li>
+          <li>
+            <span className="none">직군별 연봉 </span>
+          </li>
+          <li>
+            <span className="none">이력서 </span>
+          </li>
+          <li>
+            <span className="sup-new">커뮤니티 </span>
+          </li>
+          <li>
+            <span className="none">프리랜서 </span>
+          </li>
+          <li>
+            <span className="sup-beta">AI 합격예측 </span>
+          </li>
+        </ul>
+      </nav>
+      <div className="right-side">
+        <i className="fa-solid fa-magnifying-glass cursor"></i>
+        <span className="cursor login-button">회원가입/로그인</span>
+        <span className="bar">|</span>
+        <a href="https://www.wanted.co.kr/dashboard/welcome" className="cursor">
+          기업 서비스
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
