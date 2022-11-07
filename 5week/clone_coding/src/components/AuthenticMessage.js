348042194d3f59a5888c0f5c2c601fe8cad40fb7
Qyupang <hyunkyu0509@naver.com>
2022-11-07 16:45:28 +0900

    Feat: Add timer in authentication

--- 5week/clone_coding/src/components/AuthenticMessage.js
+++ 5week/clone_coding/src/components/AuthenticMessage.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AuthenticMessage = () => {
+  const [minutes, setMinutes] = useState(5);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      if (parseInt(seconds) > 0) {
+        setSeconds(parseInt(seconds) - 1);
+      }
+      if (parseInt(seconds) === 0) {
+        if (parseInt(minutes) === 0) {
+          clearInterval(countdown);
+        } else {
+          setMinutes(parseInt(minutes) - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => clearInterval(countdown);
+  }, [minutes, seconds]);
+
   return (
     <div className="authenticated-message">
       <h3>인증번호가 요청되었습니다.</h3>
-      <h3>유효시간 5:00</h3>
+      <h3>
+        유효시간 {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+      </h3>
     </div>
   );
 };
