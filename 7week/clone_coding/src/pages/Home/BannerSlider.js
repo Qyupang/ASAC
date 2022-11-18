85c5d7e8ea578658c0ba8b3e82205b97e6ce9698
Qyupang <hyunkyu0509@naver.com>
2022-11-19 03:58:08 +0900

    Feat: add detail on mediaquery

--- 7week/clone_coding/src/pages/Home/BannerSlider.js
+++ 7week/clone_coding/src/pages/Home/BannerSlider.js
@@ -13,7 +13,7 @@ const BannerSlider = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     speed: 1000, // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
-    arrows: true, // 옆으로 이동하는 화살표 표시 여부
+    arrows: false, // 옆으로 이동하는 화살표 표시 여부
     dots: false, // 스크롤바 아래 점으로 페이지네이션 여부
     autoplay: true, // 자동 스크롤 사용 여부
     autoplaySpeed: 2000, // 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
--- 7week/clone_coding/src/pages/Home/InsightButtonSlider.js
+++ 7week/clone_coding/src/pages/Home/InsightButtonSlider.js
@@ -9,7 +9,7 @@ const InsightButtonSlider = () => {
   const settings = {
     slide: '.career-insight__button', //슬라이드 되어야 할 태그
     variableWidth: true,
-    arrows: true, // 옆으로 이동하는 화살표 표시 여부
+    arrows: false, // 옆으로 이동하는 화살표 표시 여부
     dots: false,
     draggable: true, //드래그 가능 여부
   };
